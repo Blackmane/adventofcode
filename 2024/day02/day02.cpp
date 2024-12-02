38b652f9b2ab13fcff0b8c4999e03aae1ce5db76
Blackmane <6045018+Blackmane@users.noreply.github.com>
2024-12-02 07:48:25 +0100

    Solution 2024 day02

---
+++ 2024/day02/day02.cpp
@@ -0,0 +1,137 @@
+/**
+ * @file    day02.cpp
+ * @project advent of code
+ *
+ * @author  Niccolò Pieretti
+ * @date    02 Dec 2024
+ *
+ ****************************************************************************
+ *
+ *             _  _   o   __  __   __    _  o   _   ,_    _
+ *            / |/ |  |  /   /    /  \_|/ \_|  |/  /  |  |/
+ *              |  |_/|_/\__/\___/\__/ |__/ |_/|__/   |_/|__/
+ *                                    /|
+ *                                    \|
+ ****************************************************************************/
+
+#include "day02.h"
+
+#include "common.h"
+
+bool safe(const std::vector<uint64_t> &values)
+{
+    int64_t diff = values[0] - values[1];
+    for (size_t i = 1, count = values.size(); i < count; i++) {
+        int64_t curr_diff = values[i - 1] - values[i];
+        if (diff < 0) {
+            if (curr_diff >= 0 || curr_diff < -3) {
+                return false;
+            }
+        } else {
+            if (curr_diff <= 0 || curr_diff > 3) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool safeOneLess(const std::vector<uint64_t> &values)
+{
+    int asc = 0;
+    int desc = 0;
+    for (size_t i = 0, n = values.size(); i < n; i++) {
+        int64_t diff = values[i - 1] - values[i];
+        if (diff < 0) {
+            asc++;
+        }
+        if (diff > 0) {
+            desc++;
+        }
+    }
+    if (asc == desc) {
+        return false;
+    }
+
+    auto checker = [&](const std::vector<int> &indexes) {
+        for (size_t i = 1, n = indexes.size(); i < n; i++) {
+            int64_t diff = values[indexes[i - 1]] - values[indexes[i]];
+            if (asc > desc) {
+                if (diff >= 0 || diff < -3) {
+                    return false;
+                }
+            } else {
+                if (diff <= 0 || diff > 3) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+    for (int k = 0, n = values.size(); k < n; k++) {
+        std::vector<int> indexes;
+        indexes.reserve(n);
+        for (int i = 0; i < n; i++) {
+            if (i != k) {
+                indexes.push_back(i);
+            }
+        }
+        if (checker(indexes)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool safe2(const std::vector<uint64_t> &values)
+{
+    bool isSafe = true;
+    int64_t diff = values[0] - values[1];
+    for (size_t i = 1, count = values.size(); i < count; i++) {
+        int64_t curr_diff = values[i - 1] - values[i];
+        if (diff < 0) {
+            if (curr_diff >= 0 || curr_diff < -3) {
+                isSafe = false;
+            }
+        } else {
+            if (curr_diff <= 0 || curr_diff > 3) {
+                isSafe = false;
+            }
+        }
+    }
+    if (!isSafe) {
+        return safeOneLess(values);
+    }
+    return true;
+}
+
+std::string day02::process1(std::string file)
+{
+    uint64_t result = 0;
+    parse::read<uint64_t *>(
+        file, '\n',
+        [&](uint64_t *, const std::string &line) {
+            auto values = parse::getIntegers(line);
+            if (safe(values)) {
+                result++;
+            }
+        },
+        &result);
+    return std::to_string(result);
+}
+
+std::string day02::process2(std::string file)
+{
+    uint64_t result = 0;
+    parse::read<uint64_t *>(
+        file, '\n',
+        [&](uint64_t *, const std::string &line) {
+            auto values = parse::getIntegers(line);
+            if (safe2(values)) {
+                result++;
+            }
+        },
+        &result);
+    return std::to_string(result);
+}
