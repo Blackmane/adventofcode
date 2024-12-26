46dc399eba7382d74b32afd5d4ca9df15119ea97
Blackmane <6045018+Blackmane@users.noreply.github.com>
2024-12-26 11:36:29 +0100

    Improve 2024 day25

--- 2024/day25/day25.cpp
+++ 2024/day25/day25.cpp
@@ -18,65 +18,39 @@
 
 #include "common.h"
 
-#include <optional>
-
 std::string day25::process1(std::string file)
 {
-    std::vector<std::vector<int>> locks;
-    std::vector<std::vector<int>> keys;
+    std::vector<uint64_t> locks;
+    std::vector<uint64_t> keys;
 
-    std::vector<int> cur;
-    std::optional<bool> isKey;
-
+    uint64_t cur = 0;
     parse::read(file, '\n', [&](const std::string &line) {
         if (line.empty()) {
-            if (isKey.value()) {
+            if ((cur & 1) == 1) {
                 keys.push_back(cur);
             } else {
                 locks.push_back(cur);
             }
-            isKey = {};
+            cur = 0;
             return;
         }
-
-        if (!isKey.has_value()) {
-            isKey = line[0] == '.';
-            cur.resize(line.size());
-            for (auto &c : cur) {
-                if (isKey.value()) {
-                    c = 6;
-                } else {
-                    c = -1;
-                }
-            }
-        }
-
-        for (int i = 0, n = line.size(); i < n; i++) {
-            if (isKey.value() && line[i] == '.') {
-                cur[i]--;
-            }
-            if (!isKey.value() && line[i] == '#') {
-                cur[i]++;
+        for (auto c : line) {
+            cur = cur << 1;
+            if (c == '#') {
+                cur += 1;
             }
         }
     });
-    if (isKey.value()) {
+    if ((cur & 1) == 1) {
         keys.push_back(cur);
     } else {
         locks.push_back(cur);
     }
 
     uint64_t count = 0;
-    for (auto &lock : locks) {
-        for (auto &key : keys) {
-            bool fit = true;
-            for (int i = 0, n = lock.size(); i < n; i++) {
-                if (lock[i] + key[i] > 5) {
-                    fit = false;
-                    break;
-                }
-            }
-            if (fit) {
+    for (auto lock : locks) {
+        for (auto key : keys) {
+            if ((lock & key) == 0) {
                 count++;
             }
         }
